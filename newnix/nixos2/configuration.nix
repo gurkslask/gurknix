fab122d138bff9a36aa0b189b136d9aaafd8f9f5
Alexander Svensson <gurkslask@gmail.com>
2025-06-05 10:02:38 +0200

    Spelling

--- newnix/nixos2/configuration.nix
+++ newnix/nixos2/configuration.nix
@@ -91,7 +91,7 @@
 
   # Enable sound with pipewire.
   # sound.enable = true; Removed
-  servies.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
