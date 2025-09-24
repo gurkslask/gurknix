e0780014085f1ef38e971f1e76e372e02b53fa9b
Alexander Svensson <gurkslask@gmail.com>
2025-09-24 10:08:00 +0200

    Changed so server doesnt have kdeconnect

--- newnix/flake.nix
+++ newnix/flake.nix
@@ -97,7 +97,7 @@
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
           # > Our main home-manager configuration file <
-          ./home-manager/home.nix
+          ./home-manager/home_server.nix
         ];
       };
       "alex@nixos" = home-manager.lib.homeManagerConfiguration {
