1678a7a109298e95d57c67d0bd19dbd08b738aec
alexander svensson <gurkslask@gmail.com>
2026-03-20 08:11:46 +0100

    Fixed keymaps

--- dotfiles/nvim/lua/config/telescope.lua
+++ dotfiles/nvim/lua/config/telescope.lua
@@ -14,20 +14,32 @@ telescope.setup({
 
 telescope.load_extension("file_browser")
 
-wk.register({
-	T = { builtin.builtin, "Telescope - find picker" },
-	f = {
-		name = "Telescope",
-		f = { builtin.find_files, "Find file" },
-		g = { builtin.live_grep, "Live grep" },
-		b = { builtin.buffers, "Buffers" },
-		h = { builtin.help_tags, "Help tags" },
-		t = { builtin.treesitter, "Treesitter" },
-		r = { builtin.lsp_references, "References" },
-		c = { builtin.commands, "Commands" },
-		e = { telescope.extensions.file_browser.file_browser, "File browser" },
-	},
-}, { prefix = "<leader>" })
+-- wk.register({
+	-- T = { builtin.builtin, "Telescope - find picker" },
+	-- f = {
+		-- name = "Telescope",
+		-- f = { builtin.find_files, "Find file" },
+		-- g = { builtin.live_grep, "Live grep" },
+		-- b = { builtin.buffers, "Buffers" },
+		-- h = { builtin.help_tags, "Help tags" },
+		-- t = { builtin.treesitter, "Treesitter" },
+		-- r = { builtin.lsp_references, "References" },
+		-- c = { builtin.commands, "Commands" },
+		-- e = { telescope.extensions.file_browser.file_browser, "File browser" },
+	-- },
+-- }, { prefix = "<leader>" })
+wk.add({
+  { "<leader>T", builtin.builtin, desc = "Telescope - find picker" },
+  { "<leader>f", group = "Telescope" }, -- Definierar gruppen
+  { "<leader>ff", builtin.find_files, desc = "Find file" },
+  { "<leader>fg", builtin.live_grep, desc = "Live grep" },
+  { "<leader>fb", builtin.buffers, desc = "Buffers" },
+  { "<leader>fh", builtin.help_tags, desc = "Help tags" },
+  { "<leader>ft", builtin.treesitter, desc = "Treesitter" },
+  { "<leader>fr", builtin.lsp_references, desc = "References" },
+  { "<leader>fc", builtin.commands, desc = "Commands" },
+  { "<leader>fe", function() require('telescope').extensions.file_browser.file_browser() end, desc = "File browser" },
+})
 
 local ok, yaml_companion = pcall(require, "yaml-companion")
 if ok then
